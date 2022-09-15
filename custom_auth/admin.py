7fabd2e18c77061112704fb25f79820cb9971515
HENISH PATEL <81601957+henishpatel9045@users.noreply.github.com>
2022-09-16 00:06:50 +0530

    Securing admin panel (#2)
    * Apps creted.
    
    * Custom auth app created.
    
    * Common detail for user can be updated directly.
    Common fields of authUser can be updated from child models
    i.e. Member, Librarien.
    
    * Filtered result in child classes.
    Dropdown user field in child class is showing filtered results.
    
    * Library app created
    
    * Basic security added.
    Librarian can view all Members and only his/her record.
    Members can only view their record.
    No one can change user assigned to Librarian or Member once created.

--- custom_auth/admin.py
+++ custom_auth/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from . import models, forms
 
+from django.db import transaction
+from .models import Librarian
+from . import models, forms
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+# Register your models here.
 class UserAdmin(admin.ModelAdmin):    
     def get_form(self, req, obj, **kwargs):
         form = super().get_form(req, obj, **kwargs)
@@ -8,18 +14,44 @@ class UserAdmin(admin.ModelAdmin):
             form.base_fields['first_name'].initial = obj.user.first_name
             form.base_fields['last_name'].initial = obj.user.last_name
             form.base_fields['email'].initial = obj.user.email        
+            form.base_fields.get('user').widget.attrs['readonly'] = True
+            form.base_fields['user'].queryset = User.objects.filter(pk=obj.user.pk)
             return form
         except Exception as e:
             return form
+    
+    def get_queryset(self, request):
+        res = super().get_queryset(request)
+        user = request.user
+        if Librarian.objects.filter(user=user).exists():
+            return res
+        else:
+            return res.filter(user=user)
+    
+    def save_form(self, request, form, change):
+        with transaction.atomic():
+            user = User.objects.get(pk=request.user.pk)
+            user.first_name = form.cleaned_data.get('first_name')
+            user.last_name = form.cleaned_data.get('last_name')
+            user.email = form.cleaned_data.get('email')
+            user.save()    
+            return super().save_form(request, form, change)
 
-# Register your models here.
 @admin.register(models.Member)
 class MemberAdmin(UserAdmin):
     list_display = ['id']
     form = forms.MemberForm
     
+    def get_readonly_fields(self, request,obj=None):
+        res = ['phone_number', 'aadhaar_card_id', 'date_of_birth', 'address', 'pin_code', 'currently_borrowed_books_count', 'pending_charge']
+        return res      
+
 @admin.register(models.Librarian)
 class LibrarianAdmin(UserAdmin):
     list_display = ['id']
     form = forms.LibrarianForm
+    
+    def get_queryset(self, request):
+        return super().get_queryset(request).filter(user=request.user)
+    
     
--- custom_auth/forms.py
+++ custom_auth/forms.py
@@ -1,41 +1,21 @@
 from django import forms
 from .models import Member, Librarian
-from django.db import transaction
-from django.contrib.auth import get_user_model
-from django.db.models import Q
 
 class CustomUserForm(forms.ModelForm):
     first_name = forms.CharField(max_length=50, required=False)
     last_name = forms.CharField(max_length=50, required=False)
     email = forms.EmailField()
-    
-    class Meta:
-        fields = ['user', 'first_name', 'last_name', 'email']        
-    
-    def save(self, *args, **kwargs):
-        with transaction.atomic():
-            user = get_user_model().objects.get(pk=self.cleaned_data.get('user').pk)
-            user.first_name = self.cleaned_data.get('first_name')
-            user.last_name = self.cleaned_data.get('last_name')
-            user.email = self.cleaned_data.get('email')
-            user.save()    
-            return super().save(*args, **kwargs)
         
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        User = get_user_model()
-        self.fields['user'].queryset = User.objects.exclude(Q(pk__in=Member.objects.all().values('user')) 
-                                                            | Q(pk__in=Librarian.objects.all().values('user')))
-    
+            
 class MemberForm(CustomUserForm):
     class Meta():
         model = Member
-        fields = CustomUserForm.Meta.fields
+        fields = ['user', 'first_name', 'last_name', 'email']        
         fields += ['phone_number', 'aadhaar_card_id', 'date_of_birth', 'address', 'pin_code', 
                   'currently_borrowed_books_count', 'pending_charge']
         
 class LibrarianForm(CustomUserForm):
     class Meta():
         model = Librarian    
-        fields = CustomUserForm.Meta.fields
+        fields = ['user', 'first_name', 'last_name', 'email']        
         fields += ['phone_number', 'address'] 
